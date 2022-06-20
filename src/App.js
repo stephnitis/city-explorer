ae931557ddd8a64388c8bc905b47f524f7e49fd8
Stephanie Hill <stephni.hill@gmail.com>
2022-06-20 15:29:31 -0700

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+  }
+
+render () {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>City Explorer</h1>
+    <form>
+      <button>Explore!</button>
+    </form>
+    </>
   );
+
 }
+
+}
+
 
 export default App;
