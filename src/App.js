dc3aa16e1af544d4a9e26b99cb60d8acde1c5108
Stephanie Hill <stephni.hill@gmail.com>
2022-06-20 17:33:54 -0700

    things are happening

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
-    };
+      searchQuery: '',
+      location: {}
+    }
   }
 
-handleSubmit = (event) => {
-  event.preventDefault();
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    });
+  }
 
-}
+  handleExplore = async (event) => {
+    event.preventDefault();
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+    let cityInput = await axios.get(url);
+    console.log(cityInput.data)
+    this.setState({ location:cityInput.data[0]});
+  }
 
-render () {
-
+render() {
+  console.log(this.state.city);
   return (
     <>
-    <h1>City Explorer</h1>
-    <form onSubmit={this.handleSubmit}>
-      <button type="submit">Explore!</button>
-    </form>
+      <h1>City Explorer</h1>
+      <form onSubmit={this.handleExplore}>
+        <label>Pick a City
+          <input type="text" onInput={this.handleCityInput} />
+        </label>
+        <button type="submit">Explore!</button>
+      </form>
     </>
   );
 
 }
-
 }
 
 
