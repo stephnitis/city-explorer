75d409b5a32b2ef31d0912c2553f25f97023ca11
Stephanie Hill <stephni.hill@gmail.com>
2022-06-20 20:30:06 -0700

    image renders but with errors

--- src/App.js
+++ src/App.js
@@ -1,45 +1,74 @@
 import React from 'react';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+// import Container from 'react-bootstrap/Container';
 import './App.css';
+import Map from './Map.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchQuery: '',
-      location: {}
+      location: {},
+      mapImg: '',
+      lat: '',
+      lon: '',
     }
   }
 
   handleCityInput = (event) => {
     this.setState({
-      city: event.target.value
+      searchQuery: event.target.value
     });
   }
 
+
+
   handleExplore = async (event) => {
     event.preventDefault();
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.searchQuery}&format=json`;
+
     let cityInput = await axios.get(url);
     console.log(cityInput.data)
-    this.setState({ location:cityInput.data[0]});
+
+    this.setState({
+      location: cityInput.data[0],
+      lon: cityInput.data[0].lon,
+      lat: cityInput.data[0].lat
+    });
   }
 
-render() {
-  console.log(this.state.city);
-  return (
-    <>
-      <h1>City Explorer</h1>
-      <form onSubmit={this.handleExplore}>
-        <label>Pick a City
-          <input type="text" onInput={this.handleCityInput} />
-        </label>
-        <button type="submit">Explore!</button>
-      </form>
-    </>
-  );
 
-}
+
+
+
+  render() {
+    console.log('location:', this.state.location);
+    console.log('lon', typeof(this.state.lon));
+    console.log('lat', this.state.lat);
+    console.log('mapImg', this.state.mapImg);
+    
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <Form onSubmit={this.handleExplore}>
+          <Form.Label>Pick a City</Form.Label>
+          <Form.Control type="text" onInput={this.handleCityInput}/>
+          <Button type="submit">Explore!</Button>
+        </Form>
+        <Map
+        lat={this.state.lat}
+        lon={this.state.lon}
+        searchQuery={this.state.searchQuery}
+        />
+      </>
+      
+    );
+
+  }
 }
 
 
---
+++ src/Map.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+
+class Map extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapUrl: '',
+    }
+  }
+
+  renderMap = async () => {
+    // event.preventDefault();
+    
+    let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.props.lat},${this.props.lon}&zoom=10`;
+    await axios.get(mapUrl);
+    
+    this.setState({
+      mapUrl: mapUrl
+    
+    });
+    
+  }
+
+  render() {
+  this.renderMap();
+  console.log(this.state.mapImg);
+    return(
+      <>
+     <img className="img-fluid" 
+            src={this.state.mapUrl}
+            alt={this.props.searchQuery}
+         />
+      </>
+    )
+  }
+}
+
+
+export default Map;
