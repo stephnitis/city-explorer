1f8a7e85713cc31dcb5d2496cdbca8bc0a225b19
Stephanie Hill <stephni.hill@gmail.com>
2022-06-26 15:14:32 -0700

    stylin

---
+++ src/components/Weather.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Alert from 'react-bootstrap/Alert'
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
+import Container  from 'react-bootstrap/Container';
+import { Card } from 'react-bootstrap';
+
+
+
+class Weather extends React.Component {
+
+
+  render() {
+    return (
+      <>
+      {this.props.error === true
+          ? <Alert>{this.props.errorMsg}</Alert>
+          :
+          <>
+          <Container
+          className="d-flex align-items-center justify-content-center text-center">
+            <Card
+            className='mb-3 bg-transparent text-dark'
+            border="light"
+            style={{width: '40rem'}}
+            variant="top">
+              <Card.Body>
+              <Card.Header className='mb-3 bg-white'>{this.props.displayName}
+              </Card.Header>
+            <Card.Img 
+              border="light"
+              className='fluid p-3'
+              src={this.props.mapImg}
+              alt={this.props.displayName}
+              />
+              <Card.Text>Latitude: {this.props.lat}
+              </Card.Text>
+              <Card.Text>Longitude: {this.props.lon}
+              </Card.Text>
+              <Card.Text>Date: {this.props.date}
+              </Card.Text>
+              <Card.Text>Forecast: {this.props.forecast}
+              </Card.Text>
+           </Card.Body>
+              </Card>
+            </Container>
+          </>}
+      </>
+      
+    )
+  }
+
+}
+
+
+// class Weather extends React.Component {
+
+//   render () {
+//     return(
+//       <>
+//       {this.props.error === true
+//           ? <Alert>{this.props.errorMsg}</Alert>
+//           : 
+//         <>
+//       <ListGroup variant="success"> 
+//           <ListGroup.Item>Date: {this.props.datetime}
+//           </ListGroup.Item>
+//           <ListGroup.Item>Forecast: {this.props.description}
+//           </ListGroup.Item>
+//         </ListGroup>
+//         </>}
+//       </>
+//     )
+//   }
+
+// }
+
+export default Weather;
+
