98147353d348a381cf0407135f16c3994fb9bfb8
Stephanie Hill <stephni.hill@gmail.com>
2022-06-26 16:10:26 -0700

    gituation resolution number 3587984376438967439

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,29 +5,24 @@ import { Card } from 'react-bootstrap';
 import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
 import AccordionBody from 'react-bootstrap/esm/AccordionBody';
 import Container from 'react-bootstrap/Container';
+
 class Movies extends React.Component {
 
 
-class Movies extends React.Component {
 
 
   render() {
     return (
-      <><Container className="d-flex align-items-center justify-content-center text-center">
+      <><Container className="lign-items-center">
         {this.props.movieTitles.map((film, idx) => (
 
           <Accordion
-            defaultActiveKey={idx}
-            className="d-flex align-items-center justify-content-center text-center">
+            defaultActiveKey={idx}>
             <AccordionHeader eventKey={idx}>{film.title}</AccordionHeader>
             <AccordionBody>
               <Card.Body>{film.overview}</Card.Body>
             </AccordionBody>
           </Accordion>
-          <ListGroup key={idx}>
-            <ListGroup.Item>{film.title}</ListGroup.Item>
-            <ListGroup.Item>{film.overview}</ListGroup.Item>
-          </ListGroup>
         ))
         } </Container></>)
 
